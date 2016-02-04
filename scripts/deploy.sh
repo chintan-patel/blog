cfd912e72e7ece8bfc0b7d811051f9b075572f0b
PATEC036 <chintankumar.x.patel.-nd@disney.com>
2016-02-03 23:23:03 -0500

    Generated Blog post

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,7 @@
 DEPLOY_DIR="~/Learn-it/chintan-patel.github.io/"
 buster generate --dir $DEPLOY_DIR 
 cd $DEPLOY_DIR
+pwd
 git add -A
 git commit -m "Generated Blog post"
-git push origin master
+git push 
