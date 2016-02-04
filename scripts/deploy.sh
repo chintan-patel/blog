0d9964382adc088ee5cd932425e965ca5f6192d8
PATEC036 <chintankumar.x.patel.-nd@disney.com>
2016-02-03 23:26:02 -0500

    Generated Blog post

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-echo $HOME
-DEPLOY_DIR="~/Learn-it/chintan-patel.github.io/"
+DEPLOY_DIR="$HOME/Learn-it/chintan-patel.github.io/"
 buster generate --dir $DEPLOY_DIR 
 cd "($DEPLOY_DIR)"
 git add -A
