c5e90c96cf97096e71797a7034f2deda722499d5
Chintan patel <chintspassion@gmail.com>
2016-02-15 23:20:06 -0500

    - Added .gitignore

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-DEPLOY_DIR="$HOME/Learn-it/chintan-patel.github.io/"
-buster generate --dir $DEPLOY_DIR 
-cd $DEPLOY_DIR
-pwd
-git add -A
-git commit -m "Generated Blog post"
-git push 
+npm start &
+pip install buster
+buster setup --gh-repo=git@github.com:chintan-patel/chintan-patel.github.io.git
+buster generate 
+buster deploy
