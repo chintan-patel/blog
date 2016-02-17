df05b94ceae88df837218694df8f0c1a4c98db75
Chintan patel <chintspassion@gmail.com>
2016-02-16 23:59:47 -0500

    - Added deployed changes and new blog post

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,8 @@ npm start --production &
 pip install buster
 buster setup --gh-repo=git@github.com:chintan-patel/chintan-patel.github.io.git
 buster generate 
-buster deploy
 ./node_modules/ghost-sitemap/index.js generate
 ./node_modules/ghost-sitemap/index.js ping all 
+cp sitemap/sitemap.xml static/
+buster deploy
 pgrep node index | xargs kill
