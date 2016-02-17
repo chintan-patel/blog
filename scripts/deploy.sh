4033ee89cca96643f29b71c1bb2fb0d0598005a8
Chintan patel <chintspassion@gmail.com>
2016-02-16 23:16:34 -0500

    - Added Sitemaps

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-npm start &
+npm start --production &
 pip install buster
 buster setup --gh-repo=git@github.com:chintan-patel/chintan-patel.github.io.git
 buster generate 
 buster deploy
-pgrep node index | xargs sudo kill
+./node_modules/ghost-sitemap/index.js generate
+./node_modules/ghost-sitemap/index.js ping all 
+pgrep node index | xargs kill
